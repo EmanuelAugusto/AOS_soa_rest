4d3112b03d1c20dbd2d45ebd55779689072832fb
EmanuelAugusto <emanuel02augusto@gmail.com>
2022-04-23 11:34:59 -0300

    commit inicial

---
+++ src/main/java/com/project/professor/allocation/emanuelaugusto/ProfessorAllocationEmanuelAugustoApplication.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.emanuelaugusto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationEmanuelAugustoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationEmanuelAugustoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/professor/allocation/emanuelaugusto/ProfessorAllocationEmanuelAugustoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.emanuelaugusto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationEmanuelAugustoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
